5b27ea0ca92dcfc162b4985b8732211da0e9594d
Eduardo Alvarado <ealvara73@gmail.com>
2023-05-24 17:15:53 -0500

    Create initial inbox page

--- src/components/inbox/dom/inbox-dom.js
+++ src/components/inbox/dom/inbox-dom.js
@@ -1,11 +1,20 @@
 import newTaskButton from '../../tasks/dom/task-dom';
 import './inbox-dom.scss';
 
+const inboxInfo = () => {
+  const element = document.createElement('div')
+  element.id = 'inbox-info';
+  element.innerHTML = 'There are No Tasks coming up.';
+
+  return element;
+};
+
 const inboxPage = () => {
   const element = document.createElement('div');
   element.id = 'inbox-page';
 
   element.appendChild(newTaskButton());
+  element.appendChild(inboxInfo());
 
   return element;
 };
